d2e87c172e867cb9db16066ec3a1af0413a76f8c
Zhengri Fan <zf002@bucknell.edu>
2015-03-25 20:19:56 -0400

    121

--- src/hw02/View/WaveFormComponent.java
+++ src/hw02/View/WaveFormComponent.java
@@ -31,11 +31,13 @@ public class WaveFormComponent extends JComponent {
     private short[] rawWave;
     private int startIdx;
     private int endIdx;
+    private double amplifier;
 
     public WaveFormComponent() {
         this.rawWave = hw02.Model.SoundBasic.genTone.generatePureTone(400, 0.5, 3, hw02.Model.SoundBasic.genTone.ToneType.SINE);
         this.startIdx = 0;
         this.endIdx = 1000;
+        this.amplifier = 1;
     }
 
     public WaveFormComponent(short[] rawWave, int startIdx, int endIdx) {
@@ -66,6 +68,18 @@ public class WaveFormComponent extends JComponent {
         return endIdx;
     }
 
+    public double getAmplifier() {
+        return amplifier;
+    }
+
+    public void setAmplifier(double amplifier) {
+        this.amplifier = amplifier;
+    }
+
+    public void zoom(double percentage) {
+        this.amplifier *= (1 + percentage);
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
@@ -77,8 +91,8 @@ public class WaveFormComponent extends JComponent {
         g2d.draw(lineToRender);
         g2d.setColor(Color.BLUE);
         for (int i = this.startIdx; i < this.endIdx; ++i) {
-            int yValue = (int) ((((double) this.getHeight() / 2) / Short.MAX_VALUE) * this.rawWave[i]);
-            int xValue = (int) ((this.getWidth() / (double) (this.endIdx - this.startIdx)) * i);
+            int yValue = (int) (((((double) this.getHeight() / 2) / Short.MAX_VALUE) * this.rawWave[i]) * this.amplifier);
+            int xValue = (int) (((this.getWidth() / (double) (this.endIdx - this.startIdx)) * i) * this.amplifier);
             curPoint = new Point(xValue, yValue + this.getHeight() / 2);
             lineToRender = new Line2D.Double(prevPoint, curPoint);
             g2d.draw(lineToRender);
